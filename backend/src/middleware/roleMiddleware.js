258475c65bd6e98615aafd3c59afd4a211dba33f
Davis Kamkwamba <kamkwambadavis@gmail.com>
2025-11-19 17:55:42 +0200

    Separated Backend

--- backend/src/middleware/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-﻿const requireAdmin = (req, res, next) => {
-  if (!req.user || req.user.role !== "admin") {
-    return res.status(403).json({ data: null, error: "Admin privileges are required" });
-  }
-
-  return next();
-};
-
-module.exports = { requireAdmin };
